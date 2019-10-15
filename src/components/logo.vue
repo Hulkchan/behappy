8fa23cb6829cd562fffbd3b5771f2504a7c53b4b
chenqi <chenqihulk@163.com>
2019-10-15 18:43:09 +0800

    docker配置

---
+++ src/components/logo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="logo" title="黄靖雯我一直有在想你">
+    HULK
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {}
+  }
+}
+
+</script>
+<style lang='css' scoped='scoped'>
+  .logo{
+    font-size: 72px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    color: #fff;
+    background-color: #333;
+    padding: 0 30px;
+    cursor: pointer;
+  }
+</style>
